1fbb2cd5ed049cc781c33a5e128abb55ec116def
vagrant <vagrant@dominos.com.au>
2014-04-20 19:39:17 -0700

    Enyu : supplement tests.

---
+++ DPE.ExceptionFilter/DEP.ExceptionFilter.Tests/ExceptionFilterTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace DEP.ExceptionFilter.Tests
+{
+    [TestFixture]
+    public class ExceptionFilterTests
+    {
+        [Test]
+        public void should_log_error_instead_of_throw_exception()
+        {
+           
+
+        }
+    }
+}
+
